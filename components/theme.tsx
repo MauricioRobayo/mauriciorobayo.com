fde4442648ce5ea92ed358df39dbbf5b25f94795
Mauricio Robayo <rfmajo@gmail.com>
2023-01-29 21:26:19 -0500

    remove unnecessary styles

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,9 +1,23 @@
 import type { NextraThemeLayoutProps } from "nextra";
+import Head from "next/head";
 
-export default function Layout({ children }: NextraThemeLayoutProps) {
+export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
+  console.log(pageOpts);
+  const { title, frontMatter, headings } = pageOpts;
+
   return (
     <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="og:image" content={frontMatter.image} />
+      </Head>
       <h1>My Theme</h1>
+      Table of Contents:
+      <ul>
+        {headings.map((heading) => (
+          <li key={heading.value}>{heading.value}</li>
+        ))}
+      </ul>
       <div style={{ border: "1px solid" }}>{children}</div>
     </div>
   );
