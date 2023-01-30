cbf5a73388bcc9e9020c750416abe26a591d80b4
Mauricio Robayo <rfmajo@gmail.com>
2023-01-29 21:25:58 -0500

    add typings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import { GoogleAnalytics } from 'nextjs-google-analytics'
+import { AppProps } from "next/app";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
