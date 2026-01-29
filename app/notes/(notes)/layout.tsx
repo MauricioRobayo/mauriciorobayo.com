20f92970aa75e7e16adb09dfb102b1f11788465f
Mauricio Robayo <rfmajo@gmail.com>
2026-01-29 15:01:34 -0500

    notes improvement

--- app/notes/(notes)/layout.tsx
+++ app/notes/(notes)/layout.tsx
@@ -1,3 +1,7 @@
 export default function Layout({ children }: LayoutProps<"/notes">) {
-  return <div className="prose">{children}</div>;
+  return (
+    <div className="prose px-4 sm:px-0 prose-headings:text-center prose-headings:text-balance">
+      {children}
+    </div>
+  );
 }
