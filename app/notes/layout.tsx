cfe14882a6f1f8adefb86d7acbe7edaae3980f6f
Mauricio Robayo <rfmajo@gmail.com>
2026-01-29 13:43:09 -0500

    notes footer

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,9 +2,16 @@ import Header from "@/app/Components/Header";
 
 export default function Layout({ children }: LayoutProps<"/notes">) {
   return (
-    <div className="mt-6 flex flex-col gap-22 margin-auto items-center">
-      <Header />
-      {children}
+    <div className="mt-6 flex min-h-screen flex-col margin-auto items-center">
+      <div className="flex flex-1 flex-col gap-22 margin-auto items-center">
+        <Header />
+        {children}
+      </div>
+      <footer className="mt-auto py-16">
+        <blockquote className="text-lg font-serif italic">
+          The successful warrior is the average man, with laser-like focus.
+        </blockquote>
+      </footer>
     </div>
   );
 }
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,14 +1,13 @@
+import { cacheLife } from "next/cache";
 import Link from "next/link";
 import { getAllNotes } from "@/app/notes/utils";
-import { cacheLife } from "next/cache";
 
 export default async function Page() {
   "use cache";
   cacheLife("max");
   const allNotes = await getAllNotes("app/notes/(notes)");
-  console.log("allNotes", allNotes);
   return (
-    <ul className="flex flex-col gap-2">
+    <ul className="flex flex-col gap-4">
       {allNotes.map((note) => {
         return (
           <li key={note.slug}>
