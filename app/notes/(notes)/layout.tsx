081387fe58010e81cff99e605c4e91235faf8194
Mauricio Robayo <rfmajo@gmail.com>
2026-01-29 15:07:04 -0500

    improve layout styling for notes section

--- app/notes/(notes)/layout.tsx
+++ app/notes/(notes)/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: LayoutProps<"/notes">) {
   return (
-    <div className="prose px-4 sm:px-0 prose-headings:text-center prose-headings:text-balance">
+    <div className="prose prose-headings:text-center prose-headings:text-balance">
       {children}
     </div>
   );
--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,13 +2,13 @@ import Heading from "@/app/components/Heading";
 
 export default function Layout({ children }: LayoutProps<"/notes">) {
   return (
-    <div className="mt-6 flex min-h-screen flex-col margin-auto items-center">
+    <div className="mt-6 flex min-h-screen flex-col margin-auto items-center px-4 sm:px-0">
       <div className="flex flex-1 flex-col gap-22 margin-auto items-center">
         <Heading />
         {children}
       </div>
-      <footer className="mt-auto py-16">
-        <blockquote className="text-lg font-serif italic">
+      <footer className="mt-auto py-8 sm:py-16">
+        <blockquote className="text-lg font-serif italic text-center">
           The successful warrior is the average man, with laser-like focus.
         </blockquote>
       </footer>
