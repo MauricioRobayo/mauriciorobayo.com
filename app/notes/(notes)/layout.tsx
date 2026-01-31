fed5e34a570e496e936f7730f3b9d99253bfb3c9
Mauricio Robayo <rfmajo@gmail.com>
2026-01-31 18:55:47 -0500

    prose dark mode

--- app/notes/(notes)/layout.tsx
+++ app/notes/(notes)/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: LayoutProps<"/notes">) {
   return (
-    <div className="prose prose-headings:text-center prose-headings:text-balance">
+    <div className="prose prose-headings:text-center prose-headings:text-balance dark:prose-invert">
       {children}
     </div>
   );
