3b5dfb076be56dad13210037e8d6f35d632c5167
Mauricio Robayo <rfmajo@gmail.com>
2026-01-31 19:04:43 -0500

    add codetyper rewrite

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   cacheComponents: true,
+  async rewrites() {
+    return [
+      {
+        source: '/projects/codetyper',
+        destination: 'https://codetyper-ashen.vercel.app/',
+      },
+    ];
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
