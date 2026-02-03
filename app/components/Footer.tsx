5d804af790f967ece4f4cc1d7eb01be8f0288404
Mauricio Robayo <rfmajo@gmail.com>
2026-02-03 08:00:05 -0500

    footer adjustments

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="mt-auto pt-12 pb-8 sm:pt-22 sm:pb-16 text-balance">
-      <blockquote className="text-lg font-serif italic text-center">
+    <footer className="pt-22 pb-16 text-balance">
+      <blockquote className="font-serif italic text-center">
         The successful warrior is the average man, with laser-like focus.
       </blockquote>
     </footer>
